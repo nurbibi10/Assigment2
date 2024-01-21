ab47379e3ca95640c754190ef2b5eb0754e1060b
nurbibi10 <231645@astabait.edu.kz>
2024-01-21 21:28:35 +0600

    created variables, constructors

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,15 @@
 public class Person{
+    private String name;
+    private String surname;
+    private int id;
+    private static int  couter = 1;
 
+    public Person(){
+        this.id=couter++;
+    }
+    public Person(String name, String surname) {
+        this();
+        this.name = name;
+        this.surname = surname;
+    }
 }
