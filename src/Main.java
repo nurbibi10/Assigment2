77f57392e312f433e32b7dfbf3579d74fcfd0fd4
nurbibi10 <231645@astabait.edu.kz>
2024-01-21 22:43:19 +0600

    entering data in the array

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         ArrayList<Person> people = new ArrayList<>();
+        people.add(new Employee("Asem","Bahadyr", "Curator", 100000));
+        people.add(new Student("Asel", "Kenzhetay", 2.3));
+        people.add(new Student("Nabi", "Rakhmanberdiyeva", 3.7));
+        people.add(new Employee("Maksat","Karzhaubaev", "curator", 250000.00));
+
     }
 
 }
