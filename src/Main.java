edf25a401cd2702f1b86389267152028a3ceb886
nurbibi10 <231645@astabait.edu.kz>
2024-01-21 22:44:30 +0600

    sorting array

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 public class Main {
     public static void main(String[] args) {
         ArrayList<Person> people = new ArrayList<>();
@@ -7,6 +8,8 @@ public class Main {
         people.add(new Student("Nabi", "Rakhmanberdiyeva", 3.7));
         people.add(new Employee("Maksat","Karzhaubaev", "curator", 250000.00));
 
+        Collections.sort(people);
+        Collections.reverse(people);
     }
 
 }
