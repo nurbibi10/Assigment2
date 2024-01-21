a9b046f498d22f5f920e491614931c4048d1fe7e
nurbibi10 <231645@astabait.edu.kz>
2024-01-21 22:05:52 +0600

    created setters

--- src/Employee.java
+++ src/Employee.java
@@ -7,9 +7,16 @@ public class Employee extends Person{
     public double getSalary() {
         return salary;
     }
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
     @Override
     public String getPosition() {
         return position;
+    }
+    public void setPosition(String position) {
+        this.position = position;
     }
 
 
